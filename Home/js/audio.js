c6a65ee12607a85845a1ab8ab4b0b843b871c9f4
EveLive <MJF.Mathieu@gmail.com>
2024-04-26 12:34:21 +0200

    debugged

--- Home/js/audio.js
+++ Home/js/audio.js
@@ -3,7 +3,8 @@ const audioButton = document.getElementById('toggleAudio');
 let audioFiles = ['./audio/theme1.mp3', './audio/theme2.mp3', './audio/theme3.mp3']; // Replace with your audio files
 let currentAudioIndex = Math.floor(Math.random() * audioFiles.length); // Generate a random index
 let audio = new Audio(audioFiles[currentAudioIndex]);
-audio.loop = true; // Set loop to true
+// Decrease the volume by 80%
+audio.volume = 0.2; // 20% of the original volume
 
 let isAudioPlaying = false;
 let isFirstClick = true; // Flag to track if the audio button has been clicked for the first time
@@ -20,9 +21,9 @@ audioButton.addEventListener('click', () => {
     }
     isAudioPlaying = !isAudioPlaying;
 
-    // Fade out the audio button only after the first click
+    // Hide the audio button only after the first click
     if (!isFirstClick) {
-        audioButton.style.opacity = '0';
+        document.getElementById('audioButton').style.display = 'none';
     }
 });
 
@@ -37,9 +38,4 @@ audio.addEventListener('ended', () => {
     currentAudioIndex = nextAudioIndex;
     audio.src = audioFiles[currentAudioIndex]; // Update the audio source
     audio.play(); // Play the next audio file
-    
-    // Reset opacity of the audio button after the fade-out animation
-    setTimeout(() => {
-        audioButton.style.opacity = '1';
-    }, 500); // Adjust the duration to match the CSS transition duration
 });
